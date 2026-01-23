15a14f0262beb0b8c36d475bc88e9c45df503b15
leixerzzz <leixer16@icloud.com>
2026-01-22 20:14:22 -0500

    new

---
+++ layout.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { notFound } from "next/navigation";
+import { NextIntlClientProvider } from "next-intl";
+import { locales } from "../../../lib/i18n";
+
+type Props = {
+	children: React.ReactNode;
+	params: { locale: string };
+};
+
+export default async function LocaleLayout({ children, params }: Props) {
+	const locale = params.locale;
+	if (!locales.includes(locale as any)) notFound();
+
+	const messages = (await import(`../../../messages/${locale}.json`)).default;
+
+	return (
+		<html lang={locale}>
+			<body>
+				<NextIntlClientProvider locale={locale} messages={messages}></NextIntlClientProvider>
+					{children}
+				<</NextIntlClientProvider>
+			</body>
+		</html>
+	);
+}
